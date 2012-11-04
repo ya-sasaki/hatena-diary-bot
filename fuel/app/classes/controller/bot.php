e2392f2b378bd76346e00d45782b194040f22eed
Yasuyuki Sasaki <ya.sasaki.ja@gmail.com>
2012-11-04 21:13:43 +0900

    cron access control

--- fuel/app/classes/controller/bot.php
+++ fuel/app/classes/controller/bot.php
@@ -4,12 +4,19 @@ class Controller_Bot extends Controller
 {
     public function action_index()
     {
+        $key = Input::get('key');
+        if ($_SERVER['CRON_BOT_KEY'] != $key) 
+        {
+            echo "invalid bot execute access.";
+            exit;
+        }
         $users = Model_User::find()->where('enable', 1)->order_by('post_at')->limit(10)->get();
 
         foreach ($users as $user) 
         {
             Model_Bot::bot($user);
         } 
+        echo "bot execute complete";
         exit;
     }
 }
